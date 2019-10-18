412c0b8e51536e05e9cb002b212cd0b1830db464
Khas195 <tungthanhcao1091995@gmail.com>
2019-10-18 14:21:44 +0200

    Summary: Add an arena to test out the combat and the menus entering and exiting the game with a loading scene.
    Add The Menu Scene.
    Add The Arena Scene.
    Possible navigation from Menu Scene and Arena scene is possible.
    The start game button is disabled.

---
+++ Kristoslav-project/Assets/Scripts/Ultilities/DontDestroyOnLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script keep the parent game object and its children from being destroy when loading scene.
+/// </summary>
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
