f617a148cd43e7938a4295509048a3d744ea7f97
Khas195 <tungthanhcao1091995@gmail.com>
2019-10-20 12:14:27 +0200

    Summary: Replace the old dummy with new GrayBox Model and animation

--- Kristoslav-project/Assets/Scripts/CharacterAnimatorControl.cs
+++ Kristoslav-project/Assets/Scripts/CharacterAnimatorControl.cs
@@ -18,28 +18,11 @@ public class CharacterAnimatorControl : MonoBehaviour
     [SerializeField]
     Character character;
 
-
-
-    [SerializeField]
-    List<AnimationClip> attackingClip;
-
     // Update is called once per frame
     void Update()
     {
         animator.SetFloat("MoveSpeed", hostRb.velocity.magnitude);
         animator.SetFloat("VelocityY", hostRb.velocity.y);
-    }
-    public bool IsAttackingAnimationBeingPlayed()
-    {
-        var currentClip = GetCurrentAnimationClip();
-        foreach (var clip in attackingClip)
-        {
-            if (currentClip == clip)
-            {
-                return true;
-            }
-        }
-        return false;
     }
     public void PlayAttackAnimation()
     {
