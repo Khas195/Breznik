48e265c14db3fb5efa662b2c159ce9bd173420ba
Khas195 <tungthanhcao1091995@gmail.com>
2019-12-17 13:50:17 +0100

    Add camera collision detection

---
+++ Kristoslav-project/Assets/Scripts/3D/Camera/CameraCollision.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    [SerializeField]
+    Transform host = null;
+    [SerializeField]
+    Transform cameraTrans = null;
+    [SerializeField]
+    float minsDistance = 1f;
+    [SerializeField]
+    float maxDistance = 4f;
+    [SerializeField]
+    float smoothValue = 0.1f;
+
+    Vector3 dollyDir;
+    [SerializeField]
+    [ReadOnly]
+    Vector3 dollyDirAdjusted;
+    [SerializeField]
+    [ReadOnly]
+    float currentDistance;
+    [SerializeField]
+    LayerMask checkMask;
+    void Start()
+    {
+        dollyDir = cameraTrans.localPosition.normalized;
+        currentDistance = cameraTrans.localEulerAngles.magnitude;
+    }
+    /// <summary>
+    /// Callback to draw gizmos that are pickable and always drawn.
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(host.transform.position, host.TransformPoint(dollyDir * currentDistance));
+    }
+    void Update()
+    {
+        var desiredPos = host.TransformPoint(dollyDir * currentDistance);
+        RaycastHit hit;
+
+        if (Physics.Linecast(host.transform.position, desiredPos, out hit, checkMask))
+        {
+            Debug.Log("Line Cast Hit + " + hit.collider.gameObject);
+            currentDistance = Mathf.Clamp(hit.distance, minsDistance, maxDistance);
+        }
+        else
+        {
+            currentDistance = maxDistance;
+        }
+        cameraTrans.localPosition = Vector3.Lerp(cameraTrans.localPosition, dollyDir * currentDistance, Time.deltaTime * smoothValue);
+    }
+}
