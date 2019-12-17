1df4f0dc72b0b9c79f63533e085668d86725d8ae
Khas195 <tungthanhcao1091995@gmail.com>
2019-12-17 12:24:12 +0100

    increase bloom intensity when paused

---
+++ Kristoslav-project/Assets/Scripts/Ultilities/PostProcessVolumnController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+using UnityEngine.Rendering;
+
+public class PostProcessVolumnController : MonoBehaviour
+{
+    [SerializeField]
+    [Required]
+    Volume volume = null;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    float changePercentage;
+    [SerializeField]
+    [ReadOnly]
+    float targetBloom = 0f;
+    Bloom bloomComponent;
+
+    void Start()
+    {
+        Bloom testBloom;
+        if (volume.profile.TryGet<Bloom>(out testBloom))
+        {
+            bloomComponent = testBloom;
+        }
+    }
+    public void SetBloom(float bloom)
+    {
+        targetBloom = bloom;
+    }
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        bloomComponent.intensity.value = Mathf.Lerp(bloomComponent.intensity.value, targetBloom, changePercentage);
+    }
+}
