6fe2cade59ede23118a441bd6f9f5a98bb204cc2
Khas195 <tungthanhcao1091995@gmail.com>
2020-01-11 12:31:05 +0100

    add more enum and death vfx for the monster

--- Kristoslav-project/Assets/Scripts/Ultilities/DamageTriggerEvent.cs
+++ Kristoslav-project/Assets/Scripts/Ultilities/DamageTriggerEvent.cs
@@ -7,21 +7,22 @@ using UnityEngine.Events;
 
 [Serializable]
 public class ComboDamageTrigger : UnityEvent<int> { }
-/*
-This script's name should be changed cause it is handling all Aniamtion events not just Damage Trigger Event.
-*/
 public class DamageTriggerEvent : MonoBehaviour
 {
     [SerializeField]
-    bool playSounds = false;
+    VFXResources.VFXList deathEffect;
+    [SerializeField]
+    protected GameObject host = null;
+    [SerializeField]
+    protected bool playSounds = false;
     [SerializeField]
     [ShowIf("playSounds")]
-    AudioSource swordSwingSource = null;
+    protected AudioSource attackSoundSource = null;
 
     [SerializeField]
-    UnityEvent OnDeathAnimationDone = new UnityEvent();
+    protected UnityEvent OnDeathAnimationDone = new UnityEvent();
     public ComboDamageTrigger OnDamageTrigger = new ComboDamageTrigger();
-    public void DealsDamage(int comboCount)
+    public virtual void DealsDamage(int comboCount)
     {
         Debug.Log("Deals attack called - " + comboCount);
         OnDamageTrigger.Invoke(comboCount);
@@ -31,15 +32,23 @@ public class DamageTriggerEvent : MonoBehaviour
             if (soundsSys)
             {
                 var clipToPlay = soundsSys.GetClip(SoundDictionary.SoundList.SwordSwing);
-                swordSwingSource.clip = clipToPlay;
-                swordSwingSource.Play();
+                attackSoundSource.clip = clipToPlay;
+                attackSoundSource.Play();
             }
         }
 
     }
-    public void DeathAnimationDone()
+    public virtual void DeathAnimationDone()
     {
         OnDeathAnimationDone.Invoke();
+        var vfxSys = VFXSystem.GetInstance();
+        if (vfxSys)
+        {
+            if (this.host)
+            {
+                vfxSys.PlayEffect(deathEffect, this.host.transform.position, Quaternion.identity);
+            }
+        }
     }
 }
 
